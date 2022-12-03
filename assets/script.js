f98930cf66faf745f01315c0d82811b8f562d33c
krchandler27 <krchandler27@gmail.com>
2022-12-02 23:48:59 -0700

    I got the basics of the timer set up.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,25 @@
 var startBtn = document.querySelector("#start");
+var timeEl = document.querySelector("#time");
 
-function start() {
+function startTimer() {
     document.querySelector(".intro-section").classList.add("hide");
     document.querySelector(".questions-section").classList.remove("hide");
+
+    var timeLeft = 5;
+    var timeInterval = setInterval(function () {
+
+        if (timeLeft > -1) {
+            timeEl.textContent = timeLeft + " seconds";
+            timeLeft--;
+
+        } else {
+            timeEl.textContent = "";
+            clearInterval(timeInterval);
+    alert("You are out of time! Click 'OK' to view your score.");
+    location.href = "score.html";
+        } 
+
+}, 1000);
 }
 
-
-
-
-
-
-startBtn.addEventListener("click", start)
+startBtn.addEventListener("click", startTimer);
