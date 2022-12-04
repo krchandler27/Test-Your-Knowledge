4f3dc7bd6088696dde93d60e5968b667cd2739d9
krchandler27 <krchandler27@gmail.com>
2022-12-03 18:58:11 -0700

    updated question container in js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 var startBtn = document.querySelector("#start-btn");
 var timeEl = document.querySelector("#time");
 
+
 startBtn.addEventListener("click", startTimer);
 
 function startTimer() {
     document.querySelector(".intro-section").classList.add("hide");
-    document.querySelector(".questions-section").classList.remove("hide");
     document.querySelector(".hdr-can-you-code").classList.remove("hide");
-    // document.querySelector(".next-btn").classList.remove("hide");
+    document.querySelector(".next-btn").classList.remove("hide");
 
     var timeLeft = 5;
     var timeInterval = setInterval(function () {
@@ -26,3 +26,27 @@ function startTimer() {
     }, 1000);
 }
 
+var questions = [
+    {
+        question: "what is js?",
+        answers: ["JS", "HTML", "CSS", "Java"],
+        right: "JS"
+    },
+{
+    question: 'What is considered the "muscles of coding?"'
+    answers: ['JavaScript', 'H' ]
+}
+]
+startBtn.onclick = function () {
+    document.getElementById("question-container").innerHTML =
+        `
+    
+    <div id="question">${questions[0].question}</div>
+        <div id="answer-buttons" class="btn-grid"></div>
+        <button class="btn">${questions[0].answers[0]}</button>
+        <button class="btn">${questions[0].answers[1]}</button>
+        <button class="btn">${questions[0].answers[2]}</button>
+        <button class="btn">${questions[0].answers[3]}</button>
+    `
+    
+}
