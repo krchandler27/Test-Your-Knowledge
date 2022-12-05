5be0e6dbd69bf48cac5674718ed1d7b77d7aa2ed
krchandler27 <krchandler27@gmail.com>
2022-12-05 12:14:46 -0700

    Changed the location of the questions back to html

--- assets/main-page.js
+++ assets/main-page.js
@@ -34,27 +34,3 @@ function startTimer() {
 }
 
 
-var questions = [
-    {
-        question: "what is js?",
-        answers: ["JS", "HTML", "CSS", "Java"],
-        right: "JS"
-    },
-{
-    question: 'What is considered the "muscles of coding?"'
-    answers: ['JavaScript', 'H' ]
-}
-]
-startBtn.onclick = function () {
-    document.getElementById("question-container").innerHTML =
-        `
-    
-    <div id="question">${questions[0].question}</div>
-        <div id="answer-buttons" class="btn-grid"></div>
-        <button class="btn" id = "btn-1">${questions[0].answers[0]}</button>
-        <button class="btn" id="btn-2">${questions[0].answers[1]}</button>
-        <button class="btn" id="btn-3">${questions[0].answers[2]}</button>
-        <button class="btn" id="btn-4">${questions[0].answers[3]}</button>
-    `
-
-}
